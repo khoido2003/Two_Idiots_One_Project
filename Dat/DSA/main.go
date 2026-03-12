9f9e8e5624481d17ae399ab09415bf7fad059f9b
Datlesosad <leedat104517@gmail.com>
2026-03-12 23:12:55 +0700

    backtrack

--- Dat/DSA/main.go
+++ Dat/DSA/main.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
-	arr := tree.FromSlice([]int{3, 5, 1, 6, 2, 0, 8, -1, -1, 7, 4})
+	// arr := tree.FromSlice([]int{5, 4, 8, 11, -1, 13, 4, 7, 2, -1, -1, 5, 1})
+
+	arr := tree.FromSlice([]int{1, 2})
 
 	ans := tree.LevelOrder(arr)
 	fmt.Println(ans)
+
+	res := tree.PathSum2(arr, 1)
+	fmt.Println(res)
 }
